82979fecfe3db7e500997c857df53b01810fe48d
richmondafari <afaririchmond1000@gmail.com>
2025-01-24 16:28:47 +0000

    Explored tuples and tuple array

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,17 @@ let x: any = "Hello World ";
 
 let ids: number[] = [1, 2, 3, 4, 5];
 
-ids.push("hello world")
+ids.push("hello world");
 let array: any[] = [1, true, "hello"];
+
+// Tuple
+
+let person: [number, string, boolean] = [1, "Richmond", true];
+
+// Tuple Array
+let employee: [number, string][];
+
+employee = [
+  [1, "Richmond"],
+  [2, "Afari"],
+];
