c399ecddb02b27d1262da0a046d20c2935790c2b
richmondafari <afaririchmond1000@gmail.com>
2025-01-24 16:20:13 +0000

    Learnt about basic types with typescript

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// Basic Types
+let id: number = 5;
+
+let myName: string = "Richmond Afari";
+let isPublished: boolean = true;
+let x: any = "Hello World ";
+
+let ids: number[] = [1, 2, 3, 4, 5];
+
+ids.push("hello world")
+let array: any[] = [1, true, "hello"];
